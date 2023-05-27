5de7dfbd9491f41017231b293b581aeac2215edb
Alex Michelon <alexmichelonn@gmail.com>
2023-05-27 14:40:26 -0300

    Ajuste arquivo processing_data retirando funcao de teste filter_data

--- model/processing/processing_data.py
+++ model/processing/processing_data.py
@@ -30,14 +30,6 @@ def compare_values(value1, logical_operator, value2):
         return value1 <= value2
     return False
 
-#filtra dados considerando um campo, um valor e um operador lógico
-def filter_data_old(data, key=list, logical_operator=list, value=list):
-    found_data = []
-    for line in data:
-        if compare_values(line[key], logical_operator, value):
-            found_data.append(line)
-    return  found_data 
-
 #filtra dados considerando uma lista de dicionários contendo em dicionários dados de filtragem filter_fata = [{'key': key, 'logical_operator': logical_operator, 'value': value, 'type_value': type}]
 def filter_data(data, comparisions=list):
     found_data = []
