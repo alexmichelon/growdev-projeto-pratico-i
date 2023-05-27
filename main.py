de2a5f1e104178d943bfc281e7b8f3928b3c8f71
Alex Michelon <alexmichelonn@gmail.com>
2023-05-27 14:37:35 -0300

    Criado no módulo processing funcao filter_data considerando múltiplas comparações

--- main.py
+++ main.py
@@ -9,15 +9,29 @@ list_keys_alunos = {'nome': str, 'ano': int, 'escola': str, 'nota_semestre_1': f
 file_path_alunos = 'model/data/alunos_new.txt'
 header, data = load.open_file('model/data/alunos.csv', 'r', ',')
 
+list_keys_cars = {'nome': str, 'carro': str, 'valor': float, 'cor': str, 'ano': int, 'pago': bool}
+file_path_cars = 'model/data/carros_new.txt'
+header, data = load.open_file('model/data/carros.txt', 'r', ',')
+
 file_mode = 'w'
 
-data = load.load_data_to_list_of_dicts(data, list_keys_alunos, ',')
+data = load.load_data_to_list_of_dicts(data, list_keys_cars, ',')
 
 #file = load.load_list_of_dicts_to_data(header, data, file_path_alunos, file_mode, ',')
 #print('Acabou!')
 
-range = [50000]
+range = [2]
 #dados = [0,1,2,3,4,5,6,7,8,9,10]
 data_located = processing.locate_data(data, range)
-print(f'Dados localizados:\n {data_located}')
+#print(f'Dados localizados:\n {data_located}')
 #print(f'Dados localizado:\n {data[100]}')
+
+#{'key': key, 'logical_operator': logical_operator, 'value': value, 'type_value': type}
+filters = [{'key': 'cor', 'logical_operator': '==', 'value': 'prata', 'type_value': str},
+           {'key': 'ano', 'logical_operator': '>', 'value': '2000', 'type_value': int},
+           {'key': 'pago', 'logical_operator': '==', 'value': 't', 'type_value': bool}]
+
+found_data = processing.filter_data(data, filters)
+
+print(f'Carros filtrados: \n {found_data}')
+print(header)
--- model/processing/processing_data.py
+++ model/processing/processing_data.py
@@ -13,4 +13,43 @@ def locate_data(data=list, index=list):
     except IndexError as error:
         print(f'index not found: {error}')
         return []
-    
+
+#compara dois valores com um operador lógico
+def compare_values(value1, logical_operator, value2):
+    if logical_operator == '==':
+        return value1 == value2   
+    elif logical_operator == '>':
+        return value1 > value2   
+    elif logical_operator == '<':
+        return value1 < value2   
+    elif logical_operator == '!=':
+        return value1 != value2   
+    elif logical_operator == '>=':
+        return value1 >= value2   
+    elif logical_operator == '<=':
+        return value1 <= value2
+    return False
+
+#filtra dados considerando um campo, um valor e um operador lógico
+def filter_data_old(data, key=list, logical_operator=list, value=list):
+    found_data = []
+    for line in data:
+        if compare_values(line[key], logical_operator, value):
+            found_data.append(line)
+    return  found_data 
+
+#filtra dados considerando uma lista de dicionários contendo em dicionários dados de filtragem filter_fata = [{'key': key, 'logical_operator': logical_operator, 'value': value, 'type_value': type}]
+def filter_data(data, comparisions=list):
+    found_data = []
+    for line in data:
+        for index, comparision in enumerate(comparisions):
+            if(comparision['type_value'] == bool):
+                if(str(comparision['value'])[0].upper() in ('T', 'V', '1')):
+                    comparision['value'] = True
+                else:
+                    comparision['value'] = False
+            if compare_values(line[comparision['key']], comparision['logical_operator'], comparision['type_value'](comparision['value'])) == False:
+                break
+            if index == (len(comparisions)-1):
+                found_data.append(line)
+    return  found_data 
