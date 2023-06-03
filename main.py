f9ad36a8eb025e770143729607a750d5f0ae1941
Alex Michelon <alexmichelonn@gmail.com>
2023-06-03 18:26:28 -0300

    Criação das funções  de processamento de dados, alteração das funções de carregamento

--- main.py
+++ main.py
@@ -3,35 +3,67 @@ import model.processing.processing_data as processing
 
 list_keys_compras = {'name': str, 'last_name': str, 'age': int, 'sex': str, 'purchase': float, 'year': int, 'payment': str}
 file_path_compras = 'model/data/compras_new.txt'
-header, data = load.open_file('model/data/compras_10.csv', 'r', ',')
 
 list_keys_alunos = {'nome': str, 'ano': int, 'escola': str, 'nota_semestre_1': float, 'nota_semestre_2': float, 'faltas': int, 'nota_exame': float, 'monitoria': bool}
 file_path_alunos = 'model/data/alunos_new.txt'
-header, data = load.open_file('model/data/alunos.csv', 'r', ',')
 
-list_keys_cars = {'nome': str, 'carro': str, 'valor': float, 'cor': str, 'ano': int, 'pago': bool}
-file_path_cars = 'model/data/carros_new.txt'
-header, data = load.open_file('model/data/carros.txt', 'r', ',')
+#list_keys_cars = {'nome': str, 'carro': str, 'valor': float, 'cor': str, 'ano': int, 'cidade': str, 'pago': bool, 'tempo': str}
+list_keys_cars = [str, str, float, str, int, str, bool, str]
+file_path_cars = 'model/data/carros.txt'
+file_path_cars_save = 'model/data/carros_new.csv'
 
-file_mode = 'w'
+data_separator = ','
 
-data = load.load_data_to_list_of_dicts(data, list_keys_cars, ',')
+######Testes#######
 
-#file = load.load_list_of_dicts_to_data(header, data, file_path_alunos, file_mode, ',')
-#print('Acabou!')
+#salvar dados d arquivo na lista de dicionários
+header, data = load.load_datafile_to_list_of_dicts(file_path_cars, list_keys_cars, data_separator)
+#print(data)
 
-range = [2]
-#dados = [0,1,2,3,4,5,6,7,8,9,10]
-data_located = processing.locate_data(data, range)
-#print(f'Dados localizados:\n {data_located}')
-#print(f'Dados localizado:\n {data[100]}')
+'''#salvar lista de dicionários em um arquivo
+load.save_list_of_dicts_to_datafile(header, data, file_path_cars_save, data_separator)'''
 
-#{'key': key, 'logical_operator': logical_operator, 'value': value, 'type_value': type}
-filters = [{'key': 'cor', 'logical_operator': '==', 'value': 'prata', 'type_value': str},
-           {'key': 'ano', 'logical_operator': '>', 'value': '2000', 'type_value': int},
-           {'key': 'pago', 'logical_operator': '==', 'value': 't', 'type_value': bool}]
+'''#Acesso por índice
+indexes = [1,2]
+range = indexes[2:]
+range = [0,[0,1],[0,2,1]]
+range = [[None,None,None]]
+print(range)
+type_of_test = 'Acesso por item índice'
+result = processing.locate_data_for_list_of_index(data, range)
+print(f'{type_of_test}: \n {result}')
+'''
 
-found_data = processing.filter_data(data, filters)
+'''#Seleção/Filtragem:
+#{'key': key, 'logical_operator': logical_operator, 'value': value}
+rules = [{'key': 'cor', 'logical_operator': '==', 'value': 'prata'},
+           {'key': 'ano', 'logical_operator': '>=', 'value': 50000},
+           {'key': 'pago', 'logical_operator': '==', 'value': True},
+           {'key': 'carro', 'logical_operator': '==', 'value': 'Strada'}]
+#rules = None
+type_of_test = 'Seleção/Filtragem'
+result = processing.filter_data(data, rules)
+print(f'{type_of_test}: \n {result}')'''
 
-print(f'Carros filtrados: \n {found_data}')
-print(header)
+
+'''#Projeção:
+type_of_test = 'Projeção'
+keys = ['nome', 'carro', 'valor']
+result = processing.projection_data(data, keys)
+print(f'{type_of_test}: \n {result}')'''
+
+
+'''#Atualização:
+type_of_test = 'Atualização'
+#{'key': key, 'logical_operator': logical_operator, 'value': value}
+rules = [{'key': 'valor', 'logical_operator': '<=', 'value': 50000}]
+#rules = None
+update_list=[{'tempo': 'Neveeeee'}, {'pago': False}]
+result, data_updated, count = processing.update_data_by_rule(data, rules, update_list)
+print(f'{type_of_test}: \n Records updated: {count} \n\n\n {data_updated} \n')'''
+
+'''#Agrupamento:
+type_of_test = 'Agrupamento'
+keys = 'cidade'
+result = processing.group_data_by_count(data, keys)
+print(f'{type_of_test}: \n {result}')'''
--- model/load/load_data.py
+++ model/load/load_data.py
@@ -1,52 +1,73 @@
 #abre o arquivo de dados e o divide em cabeçalho e dados
 #parâmetros:
-# - caminho do arquivo a ser aberto (file_path)
-# -modo de uso do arquivo (file_mode). Obs.: informações sobre parâmetros no arquivo INFORMATION.md
-def open_file(file_path, file_mode, data_separator):
-    file = open(file_path, file_mode) #abre o arquivo no caminho informado e da forma de uso informada
-    content = file.readlines() #transforma os dados do arquivo em uma lista
-    header = content[0].replace('\n', '').split(data_separator) #atribui a primeira linha da lista como cabeçalho
-    data = content[1:] #atribui as demais linhas da lista como dados a serem trabalhados
-    return header, data #retorna duas listas: cabeçalho e dados
+# -file_path: caminho do arquivo a ser aberto (em formato diretório: pasta/pasta_1/arquivo.csv)
+# -data_separator: separador de cada dado (campo) em cada registro do arquivo de dados
+def open_file(file_path: str, data_separator: str):
+    '''
+    Open a datafile and splits data in header and data.\n\n
+    file_patch \n path of the datafile in format ('folder/folder_1/file.csv').\n
+    data_separator\n caracter to separate each data in a record of datafile in format (',', ' ', '|', etc.)\n
+    return tuple content header (list) and data_list (list) both separated whit data_separator.
+    '''
+    file = open(file_path, 'r') #abre o arquivo do caminho informado no modo leitura ('r')
+    content = file.readlines() #lê as linhas do arquivo de dados e transforma em uma lista
+    header = content[0].replace('\n', '').split(data_separator) #atribui o primeiro registro da lista como cabeçalho
+    data_list = content[1:] #atribui os demais registros da lista como dados
+    return header, data_list #retorna uma tupla com duas listas: cabeçalho e dados
 
 #converte dados em uma lista de dicionários, 
 #parâmetros:
-# -dados do arquivo (data)
-# -chaves e valores da lista de campos do arquivo (dict_keys)
-# -separador de dados existente no arquivo (data_separador)
-def load_data_to_list_of_dicts(data, dict_keys, data_separator):
-    list = [] #inicializa a lista de dicionários
-    for d in data: #percorre cada linha da lista de dados     
-        record = d.split(data_separator)#retira cada separador de dados e atribui a linha do arquivo a uma lista (record)
-        line = {} #inicializa o dicionário que vai receber a linha lida
-        position = 0 #iterador da posição do valor a ser lido na lista (record)
-        for key, value in dict_keys.items(): #busca cada chave e valor existente na lista de campos (dict_keys)
-            if(value == bool): #se for lógico (booleano)
-                if(record[position][0].upper() in ('T', 'V', '1') ): #verifica se o dado inicia por T (True), V (Verdadeiro) ou 1 (Verdadeiro)
-                    line.setdefault(key, True) # atribui verdadeiro
+# -file_path: caminho do arquivo a ser aberto (formato diretório: pasta/pasta_1/arquivo.csv)
+# -list_type_keys: lista de str contendo o tipo de dado de cada coluna do arquivo
+# -data_separator: separador de cada dado (campo) em cada registro do arquivo de dados (formato: ',', ' ', '|', etc.)
+def load_datafile_to_list_of_dicts(file_path: str, list_type_keys: list[str], data_separator: str):
+    '''
+    Load datafile to list of dicts. Convert dynamically the datafile records by informed list of data type values\n\n
+    file_patch \n path of the datafile in format ('folder/folder_1/file.csv').\n
+    list_type_keys \n list contents type value of datafile columns\n
+    data_separator\n caracter to separate each data in a record of datafile in format (',', ' ', '|', etc.)\n
+    return a tuple content header (list of str) e data (list of dicts).
+    '''
+    header, data_list = open_file(file_path, data_separator) #abre o aquivo e atribui a primeira linha ao cabeçalho e as demais linhas para data
+    data = [] #cria e inicializa (vazia) a lista de dicionários
+    for d in data_list: #lê cada linha da lista que contêm os dados (linha lida)
+        record = d.split(data_separator)#retira cada separador de dados dos dados e atribui a linha lida no momento a uma lista (record)
+        line = {} #cria e inicializa (vazia) o dicionário que vai receber a linha lida
+        for index in range (len(list_type_keys)): #cria contador com base no tamanho da lista de tipos de dados (list_type_keys)
+            if(list_type_keys[index] == bool): #verifica se o tipo de dado da posição lida na lista de tipos de valores das colunas é lógico (bool)
+                if(record[index][0].upper() in ('T', 'V', '1') ): #verifica se o dado inicia por T (True), V (Verdadeiro) ou 1 (Verdadeiro): valores normalmente atribuidos a valores lógicos
+                    line.setdefault(header[index], True) #insere na linha lida chave (valor do cabeçalho) e valor verdadeiro (True)
                 else: #se não encontrar este tipo de caracter
-                    line.setdefault(key, False) #atribui falso
-            else: #se o valor da chave da lista não for texto (String) ou lógico (booleano)  
-                line.setdefault(key, value(record[position].replace('\n',''))) #atribui chave e valor ao dicionário retira a quebra de linha
-            position += 1 #iterar a posição do valor a ser lido na lista (record)
-        list.append(line) #após atribuir todos os valores de chave e valor da linha, salva a linha na lista de dados  
-    return list # retorna a lista de dados: cada linha dos dados é um dicionário na lista
-
+                    line.setdefault(header[index], False) #insere na linha lida chave (valor do cabeçalho) e valor falso (False)
+            else: #se o valor da chave da lista não for lógico (bool)  
+                line.setdefault(header[index], list_type_keys[index](record[index].replace('\n',''))) #insere na linha lida chave (valor do cabeçalho) e valor ao dicionário: caso tipo de dado for str, retira a quebra de linha
+        data.append(line) #adiciona o dicionário line (posições da linha lida) dentro da lista de dicionários
+    return header, data #retorna cabeçalho e a lista de dados (cada linha dos dados é um dicionário na lista)
 
 #converte uma lista de dicionários em um arquivo e o salva 
 #parâmetros: 
-# -cabeçalho da lista de dicionários (header)
-# -lista de dicionários (list_dicts)
-# -caminho do arquivo (file_path)
-# -modo de uso do arquivo (file_mode). Obs.: informações sobre parâmetros no arquivo INFORMATION.md
-# -separador de dados (data_separator)
-def load_list_of_dicts_to_data(header, list_dicts, file_path, file_mode, data_separator):
-    file = open(file_path, file_mode) #cria o arquivo no caminho informado e seta o tipo de uso do arquivo
-    file.write(str(header).strip('[]').replace("'", '').replace(' ', '') + '\n') #insere o cabeçalho no início do arquivo
-    for dict in list_dicts: #verifica cada dicionário da lista de dicionário
-        line = '' #incializa a linha que vai receber dados do dicionário da lista de dicionários
-        for value in dict.values(): #verifica cada campo e valor do dicionário da lista de dicionários
-            line += str(value) + data_separator #adiciona o valor do dicionário somado ao separador no conteúdo existente da lista
-        line = line.rstrip(line[line.rfind(data_separator)]) + '\n' #retira o último separador de dados da linha e adiciona uma quebra de linha ao final da linha
-        file.writelines(line) #insere a linha no arquivo
-    return file # retorna o arquivo
+# -header: cabeçalho do arquivo de dados
+# -list_dicts: lista de dicionários contendo do dados do arquivo de dados
+# -file_path: caminho do arquivo a ser aberto (formato diretório: pasta/pasta_1/arquivo.csv)
+# -data_separator: separador de cada dado (campo) em cada registro do arquivo de dados
+def save_list_of_dicts_to_datafile(header: list[str], list_dicts: list[dict], file_path: str, data_separator: str):
+    '''
+    Convert list of dicts in a datafile and save in informed path\n\n
+    header\n list of str contents the name of colums of datafile\n
+    list_dicts\n list of dicts where each dict is a record of datafile\n
+    file_patch \n path of the datafile in format ('folder/folder_1/file.csv').\n
+    data_separator\n caracter to separate each data in a record of datafile in format (',', ' ', '|', etc.)\n
+    '''
+    file = open(file_path, 'w') #cria o arquivo no caminho informado no formato escrita ('w')
+    file.write(str(header).strip('[]').replace("'", '').replace(' ', '') + '\n') #insere o cabeçalho na primeira linha do arquivo: converte a lista cabeçalho em uma str, retira colchetes do início e final da str e substitui aspas simpels por espaço em branco
+    for list_index, dict in enumerate(list_dicts): #lê cada dicionário da lista de dicionários buscando índice e os dados do dicionário lido
+        line = '' #inicializa a linha que vai receber dados do dicionário lido da lista de dicionários
+        for dict_index, dict_value in enumerate(dict.values()): #lê cada valor do dicionário lido da lista de dicionários buscando índice e dados do dicionário lido
+            if dict_index != (len(dict)-1): #verifica se o índice do valor lido é o último do dicionário lido
+                line += str(dict_value) + data_separator #adiciona o valor da posição do dicionário lido na linha com o separador
+            elif list_index != (len(list_dicts)-1): #veririca se o dicionário lido é o último da lista de dicionários
+                line += str(dict_value) + '\n' #último campo do dicionário: adiciona o valor do campo do dicionário lido somado a um caracter quebra de linha
+            else: #última linha lida da lista de dicionários
+                line += str(dict_value) #adiciona o valor do dicionário lido (último valor do último dicionário lido da lista de dicionários)
+        file.write(line) #insere a linha lida no arquivo de dados
+    file.close() #fecha a escrita no arquivo de dados
